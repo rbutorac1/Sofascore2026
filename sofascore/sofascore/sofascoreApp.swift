8f2fa7a9e93bac2febd9404848e0e15fe796c5b3
rbutorac1 <roko.butorac08@gmail.com>
2026-03-04 22:18:03 +0100

    Projekt i SnapKit

---
+++ sofascore/sofascore/sofascoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  sofascoreApp.swift
+//  sofascore
+//
+//  Created by akademija on 04.03.2026..
+//
+
+import SwiftUI
+
+@main
+struct sofascoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
